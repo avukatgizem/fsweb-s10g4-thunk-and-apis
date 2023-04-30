04d82ca323dcbdf585d7f459ef052c629f53e554
avukatgizem <125642428+avukatgizem@users.noreply.github.com>
2023-04-30 14:39:20 +0300

    eksik

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Item from "./components/Item";
 import FavItem from "./components/FavItem";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAnother, addFav, getFavsFromLocalStorage } from "./actions";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
+
 
 export default function App() {
-  const loading = false;
-  const current = null;
-  const favs = [];
+  const loading = useSelector((store) => store.loading);
+  const current = useSelector((store) => store.current);
+  const favs = useSelector((store) => store.favs);
+  const dispatch = useDispatch();
+  const notification = () => {
+    toast("Favorilere eklendi!");
+  };
 
-  function addToFavs() {
-  }
-
+  useEffect(() => {
+    dispatch(fetchAnother());
+    dispatch(getFavsFromLocalStorage());
+  }, []);
 
   return (
     <div className="wrapper max-w-xl mx-auto px-4">
@@ -34,19 +45,27 @@ export default function App() {
 
       <Switch>
         <Route exact path="/">
-          {loading && <div className="bg-white p-6 text-center shadow-md">YÜKLENİYOR</div>}
+          {loading && (
+            <div className="bg-white p-6 text-center shadow-md">
+              YÜKLENİYOR...
+            </div>
+          )}
           {current && <Item data={current} />}
 
           <div className="flex gap-3 justify-end py-3">
             <button
+              onClick={() => dispatch(fetchAnother())}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Başka bir tane
             </button>
             <button
-              onClick={addToFavs}
-              className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
-            >
+              onClick={() => {
+                dispatch(addFav());
+                dispatch(fetchAnother());
+                notification();
+              }}
+              className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
               Favorilere ekle
             </button>
           </div>
@@ -54,15 +73,19 @@ export default function App() {
 
         <Route path="/favs">
           <div className="flex flex-col gap-3">
-            {favs.length > 0
-              ? favs.map((item) => (
-                <FavItem key={item.key} id={item.key} title={item.activity} />
+            {favs.length > 0 ? (
+              favs.map((item) => (
+                <FavItem key={item.id} id={item.id} title={item} />
               ))
-              : <div className="bg-white p-6 text-center shadow-md">Henüz bir favoriniz yok</div>
-            }
+            ) : (
+              <div className="bg-white p-6 text-center shadow-md">
+                Favoriniz yok ! 
+              </div>
+            )}
           </div>
         </Route>
       </Switch>
+      <ToastContainer/>
     </div>
   );
 }
